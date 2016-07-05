376485798c93007a456e40bec59072c72ba30049
gandalf3 <zzyxpaw@gmail.com>
2016-07-05 00:22:51 -0700

    make camera zooming a bit nicer

--- camera.py
+++ camera.py
@@ -1,29 +1,32 @@
 import bge
 import mathutils
+from math import sqrt
+from utils import clamp, lerp
 
 
 ## TODO
 # refactor to use a class object (get rid of globals)
 # use vector from center to cursor to control pan direction
 # investigate using "pressure" to control pan speed in some way; else use timer to slowly speed up
-# use smooth zooming
-# adjust pan speed based on zoom (pan slower when closer)
-
+# adjust pan speed based on resolution as well
 
 # for debugging
 bge.render.showMouse(True)
 
-# on a scale from 0-9
-zoom_level = 5
+# number of steps of zoom
+zoom_steps = 7
+# lowest allowed camera altitude in BU
+max_zoom = 3
+# highest allowed camera altitude in BU
+min_zoom = 90
+# initial zoom level
+zoom_level = 4
 
 # inertia settings
 acceleration = .01
 damping = .96
-margin = 10
 edge_time = 1
 
-margin /= 100
-initial_height = bge.logic.getCurrentController().owner.worldPosition.z
 
 win_x = bge.render.getWindowWidth()
 win_y = bge.render.getWindowHeight()
@@ -41,19 +44,30 @@ bge.render.setMousePosition(int(win_x/2), int(win_y/2))
 
 momentum = mathutils.Vector((0,0))
 
+def zoom_to_altitude(zoom_level):
+    global acceleration
+    acceleration = pow(clamp(zoom_level/zoom_steps, .3) * sqrt(.03), 2)
+    
+    return pow(zoom_level/zoom_steps * sqrt(min_zoom-max_zoom), 2) + max_zoom
+
+target_altitude = zoom_to_altitude(zoom_level)
+
 def zoom(cont):
-    global zoom_level
+    global zoom_level, target_altitude
     own = cont.owner
     
     mouse_w_up = own.sensors['MouseWUp']
     mouse_w_down = own.sensors['MouseWDown']
     
     if mouse_w_up.positive:
-        zoom_level -= 1
-    elif mouse_w_down.positive:
-        zoom_level += 1
+        zoom_level = clamp(zoom_level-1, 0, 7)
+        target_altitude = zoom_to_altitude(zoom_level)
         
-    own.worldPosition.z = initial_height + zoom_level * 10
+    elif mouse_w_down.positive:
+        zoom_level = clamp(zoom_level+1, 0, 7)
+        target_altitude = zoom_to_altitude(zoom_level)
+        
+    own.worldPosition.z = lerp(own.worldPosition.z, target_altitude, .1)
     
 
 def pan(cont):
@@ -65,9 +79,6 @@ def pan(cont):
     
     mouse_x = mouse_pos[0]
     mouse_y = mouse_pos[1]
-    
-    x_margin = win_x * margin
-    y_margin = win_y * margin
   
     
     if (mouse_x > win_x):
