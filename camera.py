3011544d6fb224a32e0aa39a838ccb4eb5baefd4
gandalf3 <zzyxpaw@gmail.com>
2016-07-09 13:04:19 -0700

    adjust min and max camera zoom, and disable zoom-based pan speed (tended to be unpredictable, and too slow when zoomed in/too fast when zoomed out). Possibly add back later

--- camera.py
+++ camera.py
@@ -16,9 +16,9 @@ bge.render.showMouse(True)
 # number of steps of zoom
 zoom_steps = 7
 # lowest allowed camera altitude in BU
-max_zoom = 10
+max_zoom = 4
 # highest allowed camera altitude in BU
-min_zoom = 30
+min_zoom = 40
 # initial zoom level
 zoom_level = 4
 
@@ -49,7 +49,7 @@ momentum = mathutils.Vector((0,0))
 
 def zoom_to_altitude(zoom_level):
     global max_speed
-    max_speed = max(zoom_level/max_zoom, .1) * .05
+    #max_speed = max(zoom_level/max_zoom, .1) * .05
     
     return pow(zoom_level/zoom_steps * sqrt(min_zoom-max_zoom), 2) + max_zoom
 
